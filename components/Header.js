16684b2b651139db41d708e7a0d43c172359ad01
JavierCervilla <javier@cervilla.es>
2020-10-30 07:32:53 +0100

    Initial Release from npx create-next-app
     Changes to be committed:
    	new file:   components/Header.js
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   pages/index.js
    	modified:   styles/Home.module.css

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import { Row, Col, Container } from 'react-bootstrap'
+
+const Header = () => {
+    return (
+        <div>
+            <Link href='/contact'>Contáctame</Link>
+            <Link href='/projects'>Proyectos</Link>
+        </div>
+    )
+}
+
+export default Header
