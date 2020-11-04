81946e3c12d9656d5272534e080a68d2b498d459
JavierCervilla <javier@cervilla.es>
2020-11-04 17:20:09 +0100

    [+] center products·

--- components/Projects.js
+++ components/Projects.js
@@ -1,17 +1,19 @@
-import { Container, Card, Row, Col, Image } from "react-bootstrap"
+import { Container, Card, Row, Col } from "react-bootstrap"
+import Image from 'next/image'
 import Link from 'next/link'
 
 const Projects = ({ data }) => {
     return (
         <>
             <h3 style={{ padding: '.25em', width: 'auto', borderBottom: 'solid 1px #e9e5df' }}>{data.title}</h3>
-            <Container style={{ textAlign: 'center' }} >
-                <Row style={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+            <Container style={{ display: 'flex', justifyContent: 'center', textAlign: 'center' }} >
+                <Row style={{ width: '100%' }}>
                     {data.projects && data.projects.map((project, i) =>
                         (
+
                             <Col key={`projects-${project}-${i}`} sm={12} md={6} lg={4}>
                                 <Card className='bg-primary-custom' style={{ margin: '0.5em', maxWidth: '400px' }}>
-                                    <Link href={`/project/${project.url}${data.query}`}><a><Image fluid style={{ width: '100%', height: '100%' }} src={`img/projects/${project.img}`} /></a></Link>
+                                    <Link href={`/project/${project.url}${data.query}`}><a><Image width={400} height={300} src={`/img/projects/${project.img}`} /></a></Link>
                                     <Link href={`/project/${project.url}${data.query}`}><a>{project.title}</a></Link>
                                     <Link href={`${project.git}`} >
                                         <a target="_blank" rel="noreferrer">
@@ -22,6 +24,7 @@ const Projects = ({ data }) => {
                                 </Card>
 
                             </Col>
+
                         ))}
                 </Row>
             </Container >
